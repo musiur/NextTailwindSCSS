4a09f3c48af8a09f2c363adb2cfb018f2f191917
Musiur <musiur.opu@gmail.com>
2022-12-14 11:19:48 +0600

    footer modified

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
+  const date = new Date();
   return (
-    <div className="containerG">
-      Footer
+    <div className="containerG text-center font-medium border-t-2 py-5">
+          All rights reserved to <span className="font-bold"> Musiur Alam Opu</span> @{ date.getFullYear()}
     </div>
   );
 };
